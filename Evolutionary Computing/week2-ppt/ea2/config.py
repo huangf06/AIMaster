23de7eb2f85cd0f112e431cf42fd6769c75188f1
huangf06 <52687319+huangf06@users.noreply.github.com>
2023-10-27 20:10:50 +0200

    new

--- Evolutionary Computing/week2-ppt/ea2/config.py
+++
@@ -1,8 +0,0 @@
-"""Configuration parameters for this example."""
-
-DATABASE_FILE = "database.sqlite02"
-NUM_REPETITIONS = 1
-NUM_SIMULATORS = 8
-POPULATION_SIZE = 30
-OFFSPRING_SIZE = 15
-NUM_GENERATIONS = 50
